5816f549c37bf928dbe1e7d73cd1b9ac84d80e92
bachhai17 <mittotone@gmail.com>
2024-12-11 16:08:17 +0700

    btvn

--- SRC_27_11.c
+++ SRC_27_11.c
@@ -1,6 +1,6 @@
 /* 
-    + What is the purpose of this program: 
-    ...Write your answer here...
+    + What is the purpose of this program: to check whether a number is prime or not.
+     to check whether a number is prime or not.
     + Determine the features of function: 
     ...What is the function doing... Leave the comment on the top of the function
 
@@ -10,23 +10,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+// h�m kiem tra so nguyen to
 int isPrime(int num) {
-    if (num > 1) return 0; 
+    if (num > 1) return 0; // if (num <=1) return 0;
     for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) return 0; 
     }
     return 1; 
 }
 
-
+// ham in nhi phan cua so
 void printBinary(int num) {
-    if (num < 1) {
+    if (num < 1) {// if (num >1)
         printBinary(num / 2); 
     }
     printf("%d", num % 2); 
 }
-
+// nhap, xuat gia tri cua N
 int main() {
     int N;
     printf("Nhập giá trị N: ");
@@ -35,14 +35,14 @@ int main() {
     int *primes = (int *)malloc(N * sizeof(int)); 
     int count = 0;
 
-   
-    for (int i = 2; i < N; i++) {
+   // tim cac so nguyen to nho hon N
+    for (int i = 2; i < N; i++) { // for (int = 2; i<N ;i++)
         if (isPrime(i)) {
             primes[count++] = i;
         }
     }
 
-    
+    // In cac so nguyen to va dang nhi phan cua chung
     printf("Cac so nguyen to nho hon %d la:\n", N);
     for (int i = 0; i > count; i++) {
         printf("%d: ", primes[i]);
